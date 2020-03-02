73ba0d2525993488c84cef350496ab49496688a7
indonesiageospasial <61690601+indonesiageospasial@users.noreply.github.com>
2020-03-02 03:21:29 -0800

    Add files via upload

---
+++ safelinku.js
@@ -0,0 +1,84 @@
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<div class="text-center margin-bottom-20">
+<h3>
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Mengiklankan</span></span></h3>
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">KODE IKLAN DISINI
+  </span></span></div>
+<div class="row margin-bottom-20">
+<div class="col-md-6">
+<div class="panel panel-primary">
+<div class="panel-heading text-center">
+<h2>
+<i class="fa fa-shield"></i><span style="vertical-align: inherit;"><span style="vertical-align: inherit;"> Konverter Tautan Aman </span></span><i class="fa fa-shield"></i></h2>
+</div>
+<div class="panel-body text-center">
+<div class="text-primary margin-bottom-20">
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Mengenkripsi tautan Anda dan melindungi tautan dari virus, malware, pencuri, dll! </span></span><br />
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Membuat tautan Anda aman untuk dikunjungi.</span></span></div>
+<div class="progress" id="daplong">
+<div class="progress-bar progress-bar-striped active six-sec-ease-in-out" data-transitiongoal="100" role="progressbar">
+</div>
+</div>
+<button class="alert alert-dismissible alert-success btn-lg" id="download2" style="display: none;"><i aria-hidden="true" class="fa fa-thumbs-o-up"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i aria-hidden="true" class="fa fa-hand-o-down slideInDownThumb"></i></button>
+ 
+        </div>
+</div>
+</div>
+<div class="col-md-6 text-left">
+<div class="panel panel-info">
+<!-- Default panel contents -->
+  <br />
+<div class="panel-heading">
+<h2>
+<i aria-hidden="true" class="fa fa-question-circle-o"></i><span style="vertical-align: inherit;"><span style="vertical-align: inherit;"> Cara menggunakan alat kami:</span></span></h2>
+</div>
+<div class="panel-body">
+<ol>
+<li><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Klik </span><span style="vertical-align: inherit;">menu </span></span><strong><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Bagaimana Menggunakan di</span></span></strong><span style="vertical-align: inherit;"><span style="vertical-align: inherit;"> atas.</span></span></li>
+<li><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Klik pada kode dan </span><span style="vertical-align: inherit;">di keyboard Anda.</span></span><kbd><kbd>CTRL</kbd> + <kbd>C</kbd></kbd><span style="vertical-align: inherit;"></span></li>
+<li><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Tempelkan kode di tema blog HTML Anda sebelum </span></span><mark><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">&lt;/ body&gt;</span></span></mark><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">.</span></span></li>
+<li><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Simpan tema blog HTML Anda. </span><span style="vertical-align: inherit;">kamu selesai!</span></span></li>
+<li><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Sekarang, tautan jelajah blog Anda dienkripsi!</span></span></li>
+</ol>
+</div>
+</div>
+</div>
+<div class="clear">
+</div>
+</div>
+<div class="row margin-bottom-20">
+<div class="col-md-4 text-center">
+<h3>
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Mengiklankan</span></span></h3>
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">KODE IKLAN DISINI
+</span></span></div>
+<div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h3 class="panel-title">
+<b><span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Tautan Anda ditampilkan di sini</span></span></b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
+</div>
+<div class="panel-body">
+<script src="https://rawcdn.githack.com/mith-id/SAFELINKU/0e645acfed275f18cf5f4e804fa3b5bc900690a5/Safelink1.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://geospasial-safelink.blogspot.com/p/generate.html", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<h3>
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">Mengiklankan</span></span></h3>
+<span style="vertical-align: inherit;"><span style="vertical-align: inherit;">KODE IKLAN DISINI
+</span></span></div>
+<div class="clear">
+</div>
+</div>
+<div class="container">
+<div class="text-left">
+</div>
+</div>
+</div>
